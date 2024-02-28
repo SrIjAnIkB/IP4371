7c155cb3ad6e5329095b5f68d7c6314f0e1b69aa
SrijanikB <113223805+SrIjAnIkB@users.noreply.github.com>
2024-02-28 11:28:55 +0530

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,41 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname="library";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if(!$conn){  
+
+  die('Could not connect: '.mysqli_connect_error());  
+
+}  
+echo 'Connected successfully<br/>';  
+
+$u=$_POST["user"];
+$p=$_POST["pass"];
+$sql = "SELECT name,password FROM user where name= '{$u}' ";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    if ($row["name"]== $u && $row["password"]==$p)
+       {
+           echo "You have been successfully Validated";
+
+  }
+else 
+{
+  echo "Credentials Wrong, Try again";
+} 
+}
+}
+else
+ {
+   echo "User name given was not exist";
+}
+
+$conn->close();
+ header("refresh:2; url=home.html");
+?>
+
---
+++ Register.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname="library";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if(!$conn){  
+
+  die('Could not connect: '.mysqli_connect_error());  
+
+}  
+echo 'Connected successfully<br/>';  
+
+$stmt = $conn->prepare("INSERT INTO user(name,password) VALUES(?,?)");
+$stmt->bind_param("ss", $u, $p);
+// set parameters and execute
+$u=$_POST["user"];
+$p=$_POST["pass"];
+$stmt->execute();
+ echo "Record inserted successfully";  
+$stmt->close();
+$conn->close();
+header("refresh:2; url=home.html");
+?> 
